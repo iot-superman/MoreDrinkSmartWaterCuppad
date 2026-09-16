448628666cc4ed6f2551adea8ff89978d31808f4
C311-07\User <@>
2026-09-16 09:59:50 +0800

    Update minSdk to 33

--- Android/DrinkWater/app/build.gradle.kts
+++ Android/DrinkWater/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.drinkwater"
-        minSdk = 35
+        minSdk = 33
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
